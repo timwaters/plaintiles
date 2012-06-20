932673584059e1418d524c841cfa6d7d65483a13
Tim Waters <chippy2005@gmail.com>
2012-06-20 22:15:06 +0100

    basic tile maker

--- web.rb
+++ web.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, world"
+  "<h1>Plain Tile Maker</h1>This app will serve simple, plain tiles for mapping applications
+  <h2>Colors</h2>
+  <a href='http://www.imagemagick.org/script/color.php#color_names'>http://www.imagemagick.org/script/color.php#color_names </a>
+ <h2>Usage</h2>
+ http://thisurl.com/colorname/X/Y/Z.png "
+
   end
+
+get '/:hex/*' do
+ params[:hex]
+ `convert -size 256x256 xc:wheat  meep.png`
+ send_file 'meep.png'
+ end
+
