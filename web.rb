c26f3aed2b62b2bbff6b5817a2fa7258614855da
Tim Waters <chippy2005@gmail.com>
2012-06-23 21:05:21 +0100

    add license and author info

--- web.rb
+++ web.rb
@@ -20,8 +20,10 @@ get '/' do
   For example try: <a href='http://plaintiles.herokuapp.com/hotpink/1/2/3.png'>http://plaintiles.herokuapp.com/hotpink/1/2/3.png</a>  or <a href='http://plaintiles.herokuapp.com/DodgerBlue/0/0/0.png'>http://plaintiles.herokuapp.com/DodgerBlue/0/0/0.png</a> <br />
   <h2>Examples</h2>
   Here's an <a href='http://geocommons.com/maps/180483'>example of the plain tiles being used in GeoCommons.com</a>
-  <h2>#{tile_count} Cached tiles</h2>" + html_tiles
-
+  <h2>#{tile_count} Cached tiles</h2> #{html_tiles} 
+  <h2>About</h2>
+  Made by <a href='http://thinkwhere.wordpress.com'>Tim Waters</a> @tim_waters <br />
+ <a href='https://github.com/timwaters/plaintiles'>Get the code on github</a>"
 end
 
 get '/:hex/*' do
